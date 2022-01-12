8239683e8eccb0f0780f504b7b2bae1e9d3229b8
Nikunj J Parekh <parekhnikunj@gmail.com>
2022-01-11 22:15:21 -0800

    Revert "web client env var to NIKI"
    This reverts commit 91fc2432a6b9bef54e620611f212998ba019e377.

--- nikibot.py
+++ nikibot.py
@@ -11,6 +11,7 @@ from response_map import response_dict
 from triggers import get_response_key
 from dotenv import load_dotenv
 from random import choice
+from flask import Flask, request
 import os
 import openai
 
